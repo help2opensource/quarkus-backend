a85a12cb469c4e23a06d601c92c466cde1d4afc3
help2opensource <43263369+help2opensource@users.noreply.github.com>
2025-01-22 01:10:54 -0800

    Update PostResource.java

--- src/main/java/com/openshift/fordevelopers/PostResource.java
+++ src/main/java/com/openshift/fordevelopers/PostResource.java
@@ -13,7 +13,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-
 @Path("/posts")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
